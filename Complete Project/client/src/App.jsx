f5edaab592ee0c82974fffbf771040f38a161ed7
Aniket979Kanodia <kanodiaaniket979@gmail.com>
2021-10-22 17:15:32 +0530

    Began with the project

---
+++ Complete Project/client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey = 'pudxvmxctfxa';
+
+const client = StreamChat.getInstance(apiKey); 
+
+const App = () => {
+  return (
+    <div className = "app__wrapper">
+    <Chat client= {client} theme="team light"> 
+    <ChannellistContainer/>  
+    <ChannelContainer/>  
+      
+    </Chat> 
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
